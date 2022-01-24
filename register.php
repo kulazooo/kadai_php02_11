72e11536e68f4cbb2928105390725bf6c7df2dbd
kulazooo <kulapro11@gmail.com>
2022-01-24 20:04:25 +0900

    upload

--- register.php
+++ register.php
@@ -1,28 +1,36 @@
 <?php
+
+require_once('funcs.php');
+$pdo = db_conn();
+
 //フォームからの値をそれぞれ変数に代入
 $name = $_POST['name'];
 $mail = $_POST['mail'];
 $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
 
+
+
 try {
-    //ID:'root', Password: 'root'
-    $pdo = new PDO('mysql:dbname=gs_db;charset=utf8;host=localhost', 'root', 'root');
+    $dbh = new PDO($db_host, $db_id, $db_pw);
 } catch (PDOException $e) {
-    exit('DBConnectError:' . $e->getMessage());
+    $msg = $e->getMessage();
 }
 
-// try {
-//     $dbh = new PDO($pdo, $username, $password);
-// } catch (PDOException $e) {
-//     $msg = $e->getMessage();
-// }
-
 // 1. SQL文を用意
-$stmt = $pdo->prepare("INSERT INTO gs_an_table(name, mail, pass)
+$stmt = $pdo->prepare("INSERT INTO gs_test(name, mail, pass)
                         VALUES(NULL, :name, :mail, :pass, )");
 
+
+//  2. バインド変数を用意
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':mail', $mail, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':pass', $pass, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+//  3. 実行
+$status = $stmt->execute();
+
+
 //フォームに入力されたmailがすでに登録されていないかチェック
-$sql = "SELECT * FROM gs_an_table WHERE mail = :mail";
+$sql = "SELECT * FROM gs_test WHERE mail = :mail";
 $stmt = $dbh->prepare($sql);
 $stmt->bindValue(':mail', $mail);
 $stmt->execute();
@@ -32,7 +40,7 @@ if ($member['mail'] === $mail) {
     $link = '<a href="signup.php">戻る</a>';
 } else {
     //登録されていなければinsert 
-    $sql = "INSERT INTO gs_an_table(name, mail, pass) VALUES (:name, :mail, :pass)";
+    $sql = "INSERT INTO gs_test(name, mail, pass) VALUES (:name, :mail, :pass)";
     $stmt = $dbh->prepare($sql);
     $stmt->bindValue(':name', $name);
     $stmt->bindValue(':mail', $mail);
